382728d5ad13d8f5fda2c79da42d8db270ecf827
esgerova2002 <esgerovaayten2@gmail.com>
2024-07-22 18:17:37 +0400

    header-foeter-login-register-home

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Login from "./components/Login";
+import Shop from "./pages/Shop";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
+import Success from "./components/Success";
+import "./App.css";
+import Register from "./components/Register";
 
 function App() {
+  const [modalType, setModalType] = useState("");
+
+  const handleLoginClick = () => {
+    setModalType("login");
+  };
+  const handleRegisterClick = () => {
+    setModalType("register");
+  };
+
+  const handleCloseLogin = () => {
+    setModalType("");
+  };
+
+  const handleRegisterSuccess = () => {
+    setModalType("success");
+  };
+  const handleCloseSuccess = () => {
+    setModalType("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header onLoginClick={handleLoginClick} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+      {modalType === "login" && (
+        <Login
+          handleRegisterClick={handleRegisterClick}
+          onClose={handleCloseLogin}
+        />
+      )}
+      {modalType === "register" && (
+        <Register
+          onClose={handleCloseLogin}
+          onRegisterSuccess={handleRegisterSuccess}
+        />
+      )}
+      {modalType === "success" && <Success onClose={handleCloseSuccess} />}
+    </Router>
   );
 }
 
